8b65005ef56352f4f0d83b416b8f07b1badd666d
Antoine Tilloy <antoine.tilloy@gmail.com>
2013-01-26 16:38:17 +0100

    --

--- API/src/strats/StratAntoine.java
+++ API/src/strats/StratAntoine.java
@@ -1,7 +1,14 @@
 package strats;
 
+import demo.util.InflatedCompleteMarketPrices;
+
 public class StratAntoine {
 	// J'esp�re que je vais enfin pouvoir foutre quelque chose dans ma strat sans que vous effaciez tout ^^
+	
+	public static double transactionPrice(InflatedCompleteMarketPrices OB,int runnerId,double stake,String type){
+		double cost=0;
+		return cost;
+	}
 	public static void optimalUnwind(int marketId){
 		
 	}
