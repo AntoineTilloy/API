6223bd631b9b2273727f79577f090fb5869251b1
pierrebaque <pierrebaque.baque@gmail.com>
2013-01-26 15:38:56 +0100

    debug

--- API/src/basics/Basics.java
+++ API/src/basics/Basics.java
@@ -157,13 +157,13 @@ return priceLadder;
 				
 					for ( InflatedCompletePrice p: r.getPrices()) {
 						price = p.getPrice();
-						System.out.println("volume"+p.getBackAmountAvailable());
-						System.out.println("at"+p.getPrice());
-						//if(p.getBackAmountAvailable()<=0.00001){
+					//	System.out.println("volume"+p.getBackAmountAvailable());
+						//System.out.println("at"+p.getPrice());
+						if(p.getBackAmountAvailable()<=0.00001){
 							//System.out.println("volume"+p.getBackAmountAvailable());
 							//System.out.println("at"+p.getPrice());
-						//	break;
-						//}
+							break;
+						}
 							
 						lastprice=price;
 					}
