ea7bb9ed5ac26b5868f826961b5b116bdc0d8384
Jonathan Donier <jonathan.donier@gmail.com>
2013-02-14 22:33:07 +0100

    g

--- API/src/demo/APIDemo.java
+++ API/src/demo/APIDemo.java
@@ -802,7 +802,7 @@ public class APIDemo {
 			if(!isMarketSelected()){
 				System.out.println("No Market Found");
 			}else{
-				System.out.println("Market selected : "+selectedMarket.getName());
+				System.out.println("Market selected : "+ selectedMarket.getMarketDescription());
 				System.out.println("Starting at : " + selectedMarket.getMarketTime().getTime());
 				
 			}
